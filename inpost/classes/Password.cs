3761c182770343e772e6bd0c776545bc5d6d12c6
obieuz <robizaj2006@gmail.com>
2025-10-20 08:55:36 +0200

    #5, klasa User

---
+++ inpost/classes/Password.cs
@@ -0,0 +1,53 @@
+﻿using Org.BouncyCastle.Crypto;
+using Org.BouncyCastle.Crypto.Engines;
+using Org.BouncyCastle.Crypto.Modes;
+using Org.BouncyCastle.Crypto.Paddings;
+using Org.BouncyCastle.Crypto.Parameters;
+using System;
+using System.Text;
+
+namespace inpost.classes
+{
+    public class Password
+    {
+        private static readonly string keyString = "TeslaMaMałeNóżki";
+        private static readonly string ivString = "GdzieJestPiotrek";
+        byte[] key = Encoding.UTF8.GetBytes(keyString);
+        byte[] iv = Encoding.UTF8.GetBytes(ivString);
+
+        public string password;
+        public Password(string password)
+        {
+            this.password = Encrypt(password, key, iv);
+        }
+        public string Decrypt_password()
+        {
+            return Decrypt(password, key, iv);
+        }
+        static string Encrypt(string plainText, byte[] key, byte[] iv)
+        {
+            byte[] inputBytes = Encoding.UTF8.GetBytes(plainText);
+
+            IBufferedCipher cipher = new PaddedBufferedBlockCipher(
+                new CbcBlockCipher(new AesEngine()), new Pkcs7Padding());
+            cipher.Init(true, new ParametersWithIV(new KeyParameter(key), iv));
+
+            byte[] outputBytes = cipher.DoFinal(inputBytes);
+
+            return Convert.ToBase64String(outputBytes);
+        }
+
+        static string Decrypt(string base64CipherText, byte[] key, byte[] iv)
+        {
+            byte[] inputBytes = Convert.FromBase64String(base64CipherText);
+
+            IBufferedCipher cipher = new PaddedBufferedBlockCipher(
+                new CbcBlockCipher(new AesEngine()), new Pkcs7Padding());
+            cipher.Init(false, new ParametersWithIV(new KeyParameter(key), iv));
+
+            byte[] outputBytes = cipher.DoFinal(inputBytes);
+
+            return Encoding.UTF8.GetString(outputBytes);
+        }
+    }
+}
---
+++ inpost/classes/User.cs
@@ -0,0 +1,47 @@
+﻿using Org.BouncyCastle.Crypto;
+using Org.BouncyCastle.Crypto.Engines;
+using Org.BouncyCastle.Crypto.Modes;
+using Org.BouncyCastle.Crypto.Paddings;
+using Org.BouncyCastle.Crypto.Parameters;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace inpost.classes
+{
+    public class User
+    {
+        public int Id;
+        private string Name;
+        private string Email;
+        private Password Password;
+        private DateTime Date;
+        private string Role;
+        private string PhoneNumber;
+        private string ImageUrl;
+        private bool Sex;
+
+        public User(int id, string name, string email, Password password, DateTime date, string role, string phoneNumber, string imageUrl, bool sex)
+        {
+            Id = id;
+            Name = name;
+            Email = email;
+            Password = password;
+            Date = date;
+            Role = role;
+            PhoneNumber = phoneNumber;
+            ImageUrl = imageUrl;
+            Sex = sex;
+        }
+
+        private string sign_in()
+        {
+            return Role;
+        }
+
+    }
+}
