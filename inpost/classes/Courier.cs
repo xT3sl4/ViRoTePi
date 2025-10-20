598e3515a959db3a6e23ea96ac6280d767288e5a
obieuz <robizaj2006@gmail.com>
2025-10-20 09:22:45 +0200

    #5, added class Courier

---
+++ inpost/classes/Courier.cs
@@ -0,0 +1,22 @@
+﻿using Org.BouncyCastle.Utilities.Encoders;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using System.Collections.ObjectModel;
+
+namespace inpost.classes
+{
+    internal class Courier : User
+    {
+        public string Region;
+        private ObservableCollection<String> packages;
+        public Courier(int id, string name, string email, Password password, DateTime date, string role, string phoneNumber, string imageUrl, bool sex, string region) : base(id,name,email,password,date,role,phoneNumber,imageUrl,sex)
+        {
+            this.Region = region;
+        }
+    }
+}
