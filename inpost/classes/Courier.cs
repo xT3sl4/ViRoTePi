6eda13ee2843d12df2ccc23981a4d7c8acfcc471
obieuz <robizaj2006@gmail.com>
2025-10-20 09:29:52 +0200

    #5, fixed Courier class
    Replaced type of packages from String to Package

--- inpost/classes/Courier.cs
+++ inpost/classes/Courier.cs
@@ -13,7 +13,7 @@ namespace inpost.classes
     internal class Courier : User
     {
         public string Region;
-        private ObservableCollection<String> packages;
+        private ObservableCollection<Package> packages;
         public Courier(int id, string name, string email, Password password, DateTime date, string role, string phoneNumber, string imageUrl, bool sex, string region) : base(id,name,email,password,date,role,phoneNumber,imageUrl,sex)
         {
             this.Region = region;
