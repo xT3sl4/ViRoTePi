99ae7df432398c7bffae4904f40a63a3055c5159
obieuz <robizaj2006@gmail.com>
2025-10-20 09:28:53 +0200

    #5, added Client and Package classes

---
+++ inpost/classes/Client.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace inpost.classes
+{
+    public class Client : User
+    {
+        private string payment_method;
+        public Client(int id, string name, string email, Password password, DateTime date, string role, string phoneNumber, string imageUrl, bool sex) : base(id, name, email, password, date, role, phoneNumber, imageUrl, sex)
+        {   
+        }
+        private bool sign_up()
+        {
+            return true;
+        }
+    }
+}
---
+++ inpost/classes/Package.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace inpost.classes
+{
+    internal class Package
+    {
+        public string Id;
+        private string Size;
+        private string ClientId;
+        private bool Delivered = false;
+        private DateTime ArrivalDate;
+        private DateTime PickupDeadline;
+    }
+}
